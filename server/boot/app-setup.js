cc6295888bf707f076408ac9dc57342b8b5ebab6
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2016-12-22 00:47:28 +0800

    制作了一下form的组件
    参考了 Vue，后期要不要考虑把 EJS 换成 Vue 的 SSR 呢

--- server/boot/app-setup.js
+++ server/boot/app-setup.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const _ = require('lodash');
 
 module.exports = (server)=>{
   // 开启 authentication
@@ -10,6 +11,7 @@ module.exports = (server)=>{
   server.set('views', path.resolve(__dirname, '../views'));
   server.set('view engine', 'ejs');
   server.use((req, res, next)=>{
+    res.locals._ = _;
     res.locals._v = server.get('views');
     next();
   });
