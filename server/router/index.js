5e2a4e780e965d54820c144deba0ec3467cc1e41
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2016-12-28 00:11:16 +0800

    开始制作安装页面
    1.数据库设置
    2.uploader设置 -TODO
    3.管理员设置

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const path = require('path');
+const jsonfile = require('jsonfile');
+
 module.exports = (router, server)=>{
-  router.get('/', (req, res)=>{
-    res.render('index');
+  router.get('/install/admin', (req, res)=>{
+    res.render('page/install/admin');
   });
+  router.post('/install/admin', (req, res)=>{
+    var file = path.resolve(__dirname, '../config/admin.json');
+    jsonfile.writeFileSync(file, req.body, {spaces: 2});
+    res.json({success: true});
+  });
+
+  router.get('/install/datasource', (req, res)=>{
+    res.render('page/install/datasource');
+  });
+  router.post('/install/datasource', (req, res)=>{
+    var file = path.resolve(__dirname, '../datasources.json');
+    var datasources = jsonfile.readFileSync(file);
+    console.log(datasources.db.connector);
+    res.json({success: true});
+    //res.status(404).send('hehe');
+  });
+
   router.get('/status', server.loopback.status());
 };
