4595890cf10484c61065d52ab112d27bf3ba85a6
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-01-06 01:31:20 +0800

    Input 验证
    支持多种验证
    支持验证组
    
    下一步考虑单独抽离校验的逻辑

--- client/component/form/jq-form-input.js
+++ client/component/form/jq-form-input.js
@@ -4,6 +4,37 @@
       'type': 'text',
     };
 
+    var defaultValidators = {
+      username: function(){
+        this.validator = /[\u4e00-\u9fa5_a-zA-Z0-9_-]+/;
+        this.msg = '用户名必须由 中英日韩、数字、下划线、中划线构成';
+      },
+      notEmpty: function(){
+        this.validator = function(value) {
+          return value.length > 0;
+        };
+        this.msg = '不能为空';
+      },
+      number: function(){
+        this.validator = function(value) {
+          return !isNaN(value);
+        };
+        this.msg = '必须是数字';
+      },
+      integer: function(){
+        this.validator = function(value) {
+          return !isNaN(value) && _.isInteger(parseFloat(value));
+        };
+        this.msg = '必须是整数';
+      },
+      numberLimit: function(){
+        this.validator = function(value, floor, ceil) {
+          
+        };
+        this.msg = '不能为空';
+      }
+    }
+
     var Input = function(ele, opt) {
       var $ele = this.$ele = ele;
       this.opt = $.extend({}, default_opt, opt);
@@ -21,9 +52,12 @@
 
       var $group = self.$group = self.$ele.parent('.form-group');
       if(opt.vali) {
+        self.validator = getValidator(opt.vali);
         self.$ele.on('keyup', function() {
-          if(!self.validate(opt.vali)) {
+          var valiResult = self.validate();
+          if(valiResult !== true) {
             $group.addClass('has-error');
+            console.log(valiResult);
           } else {
             $group.removeClass('has-error');
           }
@@ -31,47 +65,102 @@
       }
     }
 
-    Input.defaultValidators = {
-      username: {
-        vali: function(value, name) {
-          return value == name;
-        },
-        msg: 'msg'
-      },
-    }
-    Input.prototype.validate = function(vali) {
-      var validator = null;
-      var args = [this.val()];
+    function getValidator(opt) {
+      if(opt && opt.rule && _.isArray(opt.rule)) {
+        var validators = opt.rule.map(function(o) {
+          return getValidator(o);
+        });
 
-      try{
-        var v = new RegExp(vali.slice(1, -1));
-        // 参考: http://stackoverflow.com/questions/12257703/jquery-convert-string-to-reg-exp-object
-        if(_.isRegExp(v)) {
-          vali = v;
+        return function(value) {
+          for(var i = 0; i < validators.length; i++) {
+            var result = true;
+            result = result && validators[i](value);
+
+            if(result !== true) {
+              return result;
+            }
+          }
+          return result;
         }
-      }catch(e){};
-
-      if(_.isRegExp(vali)) {
-        validator = function(value) {
-          return vali.test(value);
-        };
       }
-      if(_.isString(vali)) {
-        if(vali.indexOf('(') > 0) {
-          var splits = vali.split(/\(|\)/);
-          validator = Input.defaultValidators[splits[0]].vali;
+
+      var rule = null;
+      var msg = null;
+      if(_.isObject(opt)) {
+        rule = opt.rule;
+        msg = opt.msg;
+      } else {
+        rule = opt;
+      }
+      var validator = null;
+      var args = [];
+
+      // 若为正则，转化为正则
+      if(_.isString(rule)
+        && rule.length > 2
+        && _.startsWith(rule, '/')
+        && _.endsWith(rule, '/')) {
+        try{
+          var v = new RegExp(rule.slice(1, -1));
+          // 参考: http://stackoverflow.com/questions/12257703/jquery-convert-string-to-reg-exp-object
+          if(_.isRegExp(v)) {
+            rule = v;
+          }
+        }catch(e){};
+      }
+      // 正则作为校验器时，转为响应的函数
+      function convertRegExp(rule) {
+        if(_.isRegExp(rule)) {
+          return function(value) {
+            return rule.test(value) && value.replace(rule, '1') === '1';
+          };
+        }
+        return rule;
+      }
+      validator = convertRegExp(rule);
+
+      // 若为函数，转化为函数
+      if(_.isString(rule)) {
+        try{
+          var v = eval("(function(){return " + rule + " })()");
+          if(_.isFunction(v)) {
+            rule = v;
+            validator = v;
+          }
+        }catch(e){};
+      }
+
+      // 字符串作为校验器时，去 defaultValidators 中找对应
+      if(_.isString(rule)) {
+        // 校验器带参
+        if(rule.indexOf('(') > 0) {
+          var splits = rule.split(/\(|\)/);
+          rule = splits[0];
           args = args.concat(
             splits[1]
               .split(',')
               .map(function(arg) {
                 arg = _.trim(arg);
-                return JSON.parse(arg);
+                try {
+                  arg = JSON.parse(arg);
+                }catch(e){};
+                return arg;
               }));
-        } else {
-          validator = Input.defaultValidators[vali];
         }
+        var defaultVali = defaultValidators[rule];
+        if(_.isFunction(defaultVali)) {
+          defaultVali = new defaultVali();
+        }
+        validator = convertRegExp(defaultVali.validator);
+        msg = opt.msg || defaultVali.msg;
       }
-      return validator.apply(null, args);
+
+      return function(value) {
+        return validator.apply(null, [value].concat(args)) ? true : msg;
+      }
+    }
+    Input.prototype.validate = function() {
+      return this.validator.apply(null, [this.val()]);
     }
 
     Input.prototype.val = function() {
