9fcb49069ab0729aad4a44a4e14cfb5c33c5e9bd
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-01-14 23:49:37 +0800

    整理、注释

--- server/router/install.js
+++ server/router/install.js
@@ -6,40 +6,48 @@ const loopback = require('loopback');
 const co = require('co');
 const Promise = require('bluebird');
 
+// 调用失败时的返回错误信息
+let onError = (err, res)=>{
+  res.status(500);
+  res.json({
+    success: false,
+    msg: err.toString(),
+  });
+}
+
 module.exports = (router, server)=>{
   router.get('/install/admin', (req, res)=>{
     res.render('page/install/admin');
   });
   router.post('/install/admin', (req, res)=>{
-    var file = path.resolve(__dirname, '../config/admin.json');
+    let file = path.resolve(__dirname, '../config/admin.json');
     jsonfile.writeFileSync(file, req.body, {spaces: 2});
     res.json({success: true});
   });
 
+  // 数据库配置
   router.get('/install/datasource', (req, res)=>{
     res.render('page/install/datasource');
   });
-
-  let onError = (err, res)=>{
-    res.status(500);
-    res.json({
-      success: false,
-      msg: err.toString(),
-    });
-  }
   router.post('/install/datasource', (req, res)=>{
     co(function*() {
-      var dbConf = req.body.db;
-      var db = loopback.registry.createDataSource('db', dbConf);
+      let dbConf = req.body.db;
+      let db = loopback.registry.createDataSource('db', dbConf);
       db.ping((err)=>{
         if(err) {
           return onError(err, res);
         }
 
+        // 用 confirm 字段来区分 test 和 confirm 情况
+        // conform 的情况下需要：
+        
+        // 1.将配置写入 datasources.json 配置文件
         if(req.body.confirm) {
-          var file = path.resolve(__dirname, '../datasource.json');
+          let file = path.resolve(__dirname, '../datasource.json');
           jsonfile.writeFileSync(file, {db: dbConf}, {spaces: 2});
         }
+        // 2.重启服务
+        // TODO
 
         return res.json({
           success: true
