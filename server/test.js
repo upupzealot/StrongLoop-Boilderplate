965985a5c1afc57ea715006e1e68f5a4a7df4e96
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-02-10 03:19:25 +0800

    搭建测试环境

---
+++ server/test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const _ = require('lodash');
+const Promise = require('bluebird');
+const co = require('co');
+const dir = require('node-dir');
+const path = require('path');
+
+const loopback = require('loopback');
+const boot = require('loopback-boot');
+const app = loopback();
+
+const Mocha = require('mocha');
+const mocha = new Mocha();
+
+var testDir = path.resolve(__dirname, './test');
+
+boot(app, {}, function(err) {
+  if (err) throw err;
+
+  co(function*() {
+    let files = yield (Promise.promisify(dir.files))(testDir);
+    files.forEach((fileName)=>{
+      if(_.endsWith(fileName, '.test.js')) {
+        mocha.addFile(fileName);
+      }
+    });
+
+    mocha.run(function(failures){
+      process.on('exit', function () {
+        process.exit(failures);  // exit with non-zero status if there were failures
+      });
+    });
+  }).catch(console.err);
+});
