9b451da40b922deb18e19cc5a17d0bd88c91b429
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-02-09 23:04:37 +0800

    用户登录/注册的功能、页面
    注册后可直接登录

---
+++ server/models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const co =require('co');
+const loopback =require('loopback');
+const cookieSignature = require('cookie-signature');
+
+module.exports = (User)=>{
+  User.afterRemote('create', (ctx, instance, next)=>{
+    co(function*() {
+      let token = yield instance.createAccessToken();
+      let signature = 's:' + cookieSignature.sign(token.id, '***');
+      ctx.res.cookie('authorization' ,signature);
+      next();
+    }).catch(next);
+  });
+
+  User.beforeRemote('login', (ctx, instance, next)=> {
+    co(function*() {
+      let User = loopback.getModel('user');
+      let user = yield User.findOne({
+        where: {
+          username: ctx.args.credentials.email
+        }
+      });
+      if(user) {
+        ctx.args.credentials.email = user.email;
+      }
+      next();
+    }).catch(next);
+  });
+
+  User.afterRemote('login', (ctx, instance, next)=> {
+    co(function*() {
+      let signature = 's:' + cookieSignature.sign(instance.id, '***');
+      ctx.res.cookie('authorization' ,signature);
+      next();
+    }).catch(next);
+  });
+}
---
+++ server/router/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = (router, server)=>{
+  router.get('/register', (req, res)=>{
+    res.render('page/user/register.ejs');
+  });
+
+  router.get('/login', (req, res)=>{
+    res.render('page/user/login.ejs');
+  });
+}
