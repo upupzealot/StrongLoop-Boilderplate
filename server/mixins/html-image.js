b9c2abde553e747aea840365b88331622701857c
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-02-14 00:11:13 +0800

    HtmlImage mixin

---
+++ server/mixins/html-image.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const _ = require('lodash');
+const xss = require('xss');
+
+module.exports = (Model, options)=> {
+  const opt = _.merge({}, {
+    fromField: 'content',
+    // thumbnailField: 'thumbnail',
+    // galleryField: 'images',
+    // textField: 'text',
+  }, options);
+
+  if(opt.thumbnailField) {
+    Model.defineProperty(opt.thumbnailField, {
+      type: String,
+    });
+  }
+  if(opt.galleryField) {
+    Model.defineProperty(opt.galleryField, {
+      type: Object,
+    });
+  }
+  if(opt.textField) {
+    Model.defineProperty(opt.textField, {
+      type: String,
+    });
+  }
+
+  Model.observe('before save', (ctx, next)=>{
+    const instance = ctx.instance;
+    const html = instance[opt.fromField];
+    let thumbnail = null;
+    const images = [];
+    let text = '';
+    if(html) {
+      if(opt.thumbnailField || opt.galleryField) {
+        xss(instance[opt.fromField], {
+          onTagAttr: (tag, name, value, isWhiteAttr)=>{
+            if (tag === 'img' && name === 'src') {
+              images.push(xss.friendlyAttrValue(value));
+            }
+          }
+        });
+        thumbnail = images.length ? images[0] : null;
+      }
+
+      if(opt.textField) {
+        text = xss(html, {
+          whiteList: [],
+          stripIgnoreTag: true,
+          onTag: function (tag, html, info) {
+            if (tag === 'img') {
+              if(_.startsWith(html, '</')) {
+                return '';
+              }
+              return '[图片]';
+            }
+          }
+        });
+      }
+    }
+
+    if(opt.thumbnailField) {
+      instance[opt.thumbnailField] = thumbnail;
+    }
+    if(opt.galleryField) {
+      instance[opt.galleryField] = images;
+    }
+    if(opt.textField) {
+      instance[opt.textField] = text;
+    }
+    next();
+  });
+}
---
+++ server/test/mixins/html-image.test.js
@@ -0,0 +1,80 @@
+'use strict';
+
+require('co-mocha')(require('mocha'));
+const should = require('should');
+const loopback = require('loopback');
+
+const util = require('../lib/util');
+const shouldThrow = util.shouldThrow;
+const shouldNotThrow = util.shouldNotThrow;
+const getModel = util.getMixinModel;
+
+describe('Mixin: Rich Text Image', function() {
+  const instance = {
+      content: 
+        '<p>' +
+          '<img src="/test-img-0.jpeg">aaaaa' +
+          '<img src="/test-img-1.jpeg"></img>' +
+          '<img src="/test-img-2.jpeg"></img>' +
+        '</p>'
+    }
+
+  it('thumbnail filter', function*() {
+    const Topic = getModel('Topic', {
+      HtmlImage: {
+        thumbnailField: 'thumbnail',
+      },
+    });
+
+    let topic = yield Topic.create(instance);
+    should(topic.thumbnail).equal('/test-img-0.jpeg'); 
+  });
+
+  it('gallery filter', function*() {
+    const Topic = getModel('Topic', {
+      HtmlImage: {
+        galleryField: 'images',
+      },
+    });
+
+    let topic = yield Topic.create(instance);
+    should(topic.images).be.an.Array()
+      .which.eql([
+          '/test-img-0.jpeg',
+          '/test-img-1.jpeg',
+          '/test-img-2.jpeg'
+        ]); 
+  });
+
+  it('gallery filter', function*() {
+    const Topic = getModel('Topic', {
+      HtmlImage: {
+        textField: 'text',
+      },
+    });
+
+    let topic = yield Topic.create(instance);
+    should(topic.text).equal('[图片]aaaaa[图片][图片]'); 
+  });
+
+  describe('option', function() {
+    it('fromField', function*() {
+      const Topic = getModel('Topic', {
+        HtmlImage: {
+          fromField: 'html',
+          thumbnailField: 'aaaaa',
+        },
+      });
+
+      shouldThrow(function*() {
+        let topic = yield Topic.create(instance);
+      });
+      shouldNotThrow(function*() {
+        let topic = yield Topic.create({
+          html: instance.content
+        });
+        should(topic.aaaaa).equal('/test-img-0.jpeg'); 
+      });
+    });
+  });
+});
