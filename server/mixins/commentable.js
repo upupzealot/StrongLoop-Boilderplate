a3a0b9845c6d98c28bae7cfcd2ebb9b0392b91f0
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-02-28 00:09:47 +0800

    commentable mixin 的 commentable、replied relation

--- server/mixins/commentable.js
+++ server/mixins/commentable.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const co = require('co');
 const loopback = require('loopback');
 const app = require('../server.js');
 
@@ -21,18 +22,49 @@ module.exports = (Model, options) => {
         },
         Commentable: {},
       },
+      relations: {
+        commentable: {
+          type: 'belongsTo',
+          polymorphic: {
+            foreignKey: 'commentable_id',
+            discriminator: 'commentable_type',
+          },
+        },
+        replied: {
+          type: 'belongsTo',
+          model: 'Comment',
+          foreignKey: 'replied_id',
+        },
+      },
     });
     app.model(Comment, {
       public: true,
       dataSource: db,
+    });
+
+    Comment.observe('before save', (ctx, next) => {
+      const instance = ctx.instance;
+      if (instance && instance.commentable_type === 'Comment') {
+        co(function*() {
+          const comment = yield Comment.findById(instance.commentable_id);
+
+          instance.commentable_id = comment.commentable_id;
+          instance.commentable_type = comment.commentable_type;
+          instance.replied_id = comment.id;
+
+          next();
+        }).catch(next);
+      } else {
+        next();
+      }
     });
   }
 
   Model.hasMany('comment', {
     as: 'comments',
     polymorphic: {
-      foreignKey: 'commentableId',
-      discriminator: 'commentableType',
+      foreignKey: 'commentable_id',
+      discriminator: 'commentable_type',
     },
   });
 };
--- server/test/mixins/commentable.test.js
+++ server/test/mixins/commentable.test.js
@@ -16,7 +16,7 @@ describe('Mixin: Commentable', function () {
   beforeEach(mixinModel('Commentable', opts));
 
   describe('Relation', function () {
-    it('comments field', function*() {
+    it('comments', function*() {
       const created = yield this.Topic.create({});
       let topic = yield this.Topic.findById(created.id, {include: 'comments'});
 
@@ -26,6 +26,39 @@ describe('Mixin: Commentable', function () {
         .which.is.an.Array();
     });
 
+    describe('commentable', function () {
+      it('comment\'s commentable', function*() {
+        const topic = yield this.Topic.create({});
+        const comment = yield topic.comments.create({content: 'haha'});
+        const commentable = yield comment.commentable.getAsync();
+
+        should(commentable).be.ok();
+        should(commentable.toJSON()).eql(topic.toJSON());
+      });
+
+      it('fif\'s commentable', function*() {
+        const topic = yield this.Topic.create({});
+        const comment = yield topic.comments.create({content: 'aaa'});
+        const fif = yield comment.comments.create({content: 'bbb'});
+        const commentable = yield fif.commentable.getAsync();
+
+        should(commentable).be.ok();
+        should(commentable.toJSON()).eql(topic.toJSON());
+      });
+    });
+
+    it('replied comment', function*() {
+      const topic = yield this.Topic.create({});
+      const comment = yield topic.comments.create({content: 'aaa'});
+      const fif = yield comment.comments.create({content: 'bbb'});
+      const replied = yield fif.replied.getAsync();
+
+      should(replied).be.ok();
+      should(replied.toJSON()).eql(comment.toJSON());
+    });
+  });
+
+  describe('comments creation', function () {
     it('create comments', function*() {
       const created = yield this.Topic.create({});
       yield remote.post(`/api/Topics/${created.id}/comments`, 422);
