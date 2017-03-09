5d526add7b08c6ab0db93d88cc498dbbba421cbc
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-03-09 23:34:27 +0800

    HtmlField mixin 支持多字段

--- server/mixins/html-field.js
+++ server/mixins/html-field.js
@@ -4,20 +4,41 @@ const _ = require('lodash');
 const xss = require('xss');
 
 module.exports = (Model, options) => {
-  const opt = _.merge({}, {
-    field: 'content',
-    required: true,
-  }, options);
+  let opts = options.field;
 
-  Model.defineProperty(opt.field, {
-    type: String,
-    required: opt.required,
-    description: opt.description,
-  });
+  const parseField = function(field) {
+    if(_.isString(field)) {
+      return {
+        field,
+        required: true,
+      }
+    } else {
+      return _.merge({}, {
+        field: 'content',
+        required: true,
+      }, field);
+    }
+  }
 
-  Model.observe('before save', (ctx, next) => {
-    const instance = ctx.instance;
-    instance[opt.field] = xss(instance[opt.field]);
-    next();
+  if(_.isArray(opts)) {
+    opts = opts.map((option)=>{
+      return parseField(option);
+    });
+  } else {
+    opts = [parseField(opts)];
+  }
+
+  opts.map((opt)=>{
+    Model.defineProperty(opt.field, {
+      type: String,
+      required: opt.required,
+      description: opt.description,
+    });
+
+    Model.observe('before save', (ctx, next) => {
+      const instance = ctx.instance;
+      instance[opt.field] = xss(instance[opt.field]);
+      next();
+    });
   });
 };
