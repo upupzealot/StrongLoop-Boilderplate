45394aba2da08a12905c910f574b6890f66e8170
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-03-14 11:35:30 +0800

    test、develop 环境下 automigrate、否则 autoupdate

--- server/boot/after/auto-update.js
+++ server/boot/after/auto-update.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = (server, callback) => {
-  server.datasources.db.autoupdate(callback);
+  const db = server.datasources.db;
+  const autoupdate = (method) => {
+    if (db.connected) {
+      db[method](callback);
+    } else {
+      db.once('connected', () => {
+        db[method](callback);
+      });
+    }
+  };
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'develop') {
+    autoupdate('automigrate');
+  } else {
+    autoupdate('autoupdate');
+  }
 };
