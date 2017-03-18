d5cfd154e2b4ea30ccee4fba524e7268bef0ce74
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-03-18 23:21:18 +0800

    eslint format

--- server/mixins/viewable.js
+++ server/mixins/viewable.js
@@ -3,13 +3,13 @@
 module.exports = (Model) => {
   // TODO: other mixins
 
-  Model.listUrl = ()=>{
-    return '/' + Model.pluralModelName;
-  }
-  Model.prototype.listUrl = ()=>{
+  Model.listUrl = () => {
+    return `/${Model.pluralModelName}`;
+  };
+  Model.prototype.listUrl = () => {
     return Model.listUrl();
-  }
-  Model.prototype.url = function() {
-    return '/' + Model.pluralModelName + '/' + this.id;
+  };
+  Model.prototype.url = function () {
+    return `/${Model.pluralModelName}/${this.id}`;
   };
 };
