0fc7ecc5d0120b8888d86e1d6478523487f7dec2
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-03-18 22:12:13 +0800

    开始制作 viewable mixin

---
+++ server/mixins/viewable.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = (Model) => {
+  // TODO: other mixins
+
+  Model.listUrl = ()=>{
+    return '/' + Model.pluralModelName;
+  }
+  Model.prototype.listUrl = ()=>{
+    return Model.listUrl();
+  }
+  Model.prototype.url = function() {
+    return '/' + Model.pluralModelName + '/' + this.id;
+  };
+};
