331c2b5d6e924299e195be02729910b516f850ef
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-04-25 00:00:43 +0800

    Notification: find, count 限定当前用户

---
+++ server/models/notification.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const _ = require('lodash');
+
+const loopback = require('loopback');
+
+module.exports = (Notification) => {
+  Notification.beforeRemote('find', (ctx, instance, next)=>{
+    ctx.args.filter = ctx.args.filter || {};
+    ctx.args.filter.where = _.merge(ctx.args.filter.where || {}, {
+      to_id: ctx.args.options.accessToken.userId,
+      is_read: false,
+    });
+    next();
+  });
+
+  Notification.beforeRemote('count', (ctx, instance, next)=>{
+    ctx.args.where = _.merge(ctx.args.where || {}, {
+      to_id: ctx.args.options.accessToken.userId,
+      is_read: false,
+    });
+    next();
+  });
+};
