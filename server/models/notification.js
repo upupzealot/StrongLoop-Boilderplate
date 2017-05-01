561581cbae872758c4d9b85f916060cb8bba1320
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-05-02 02:10:44 +0800

    update: notification 拉取
    不同类型的 notification 渲染不同的 html

--- server/models/notification.js
+++ server/models/notification.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const co = require('co');
 const loopback = require('loopback');
 
 module.exports = (Notification) => {
@@ -24,36 +25,46 @@ module.exports = (Notification) => {
     reply: '回复了',
   };
 
-  Notification.prototype.getHTML = function () {
+  Notification.prototype.getHTML = function*() {
     const event = this.toJSON().event;
+    const targetModel = loopback.getModel(event.target_type);
 
-    if (event.action === 'comment') {
-      this.html =
+    if (event.action === 'comment' || event.action === 'reply') {
+      if (event.action === 'comment') {
+        this.html =
         `${event.from.username
-        } 评论了你的${loopback.getModel(event.target.commentable_type).modelName
-        }(${event.target.commentable_id})`;
-    } else if (event.action === 'reply') {
-      this.html =
+        } 评论了你的${targetModel.modelName
+        } <a href="${event.target.url}">${event.target.title
+        }</a> `;
+      }
+
+      if (event.action === 'reply') {
+        const commentableModel = loopback.getModel(event.target.commentable_type);
+        const commentable = yield commentableModel.findById(event.target.commentable_id);
+
+        this.html =
         `${event.from.username
-        } 回复了你在 ${loopback.getModel(event.target.commentable_type).modelName
-        }(${event.target.commentable_id})` +
-        ` 下的评论${
-        loopback.getModel(event.target_type).modelName
+        } 回复了你在 ${commentableModel.modelName
+        } <a href="${commentable.url}">${commentable.title
+        }</a> 下的评论${targetModel.modelName
         }(${event.target_id})`;
+      }
     } else {
       this.html =
         `${event.from.username
         } ${Actions[event.action]
-        } ${loopback.getModel(event.target_type).modelName
+        } ${targetModel.modelName
         }(${event.target_id})`;
     }
   };
 
   Notification.afterRemote('find', (ctx, instances, next) => {
-    instances.forEach((instance) => {
-      instance.getHTML();
-    });
-    next();
+    co(function*() {
+      yield instances.map(function*(instance) {
+        yield instance.getHTML();
+      });
+      next();
+    }).catch(next);
   });
 
   Notification.beforeRemote('count', (ctx, instance, next) => {
