f3f8fd93cb4e3e817bcb52591111073aac6f59bd
wb-pxn234203 <wb-pxn234203@alibaba-inc.com>
2017-05-04 00:16:25 +0800

    rename: viewable -> routable

--- server/mixins/routable.js
+++ server/mixins/routable.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = (Model) => {
-  // TODO: other mixins
-
   Model.observe('loaded', (ctx, next) => {
     const instance = ctx.instance || ctx.data;
     instance.listUrl = `/${Model.pluralModelName}`;
